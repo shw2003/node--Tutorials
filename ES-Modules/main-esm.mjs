3b6d58a4933ce4405df7612a26de7f9e29ca2a63
shweta2003 <shwetakamal4714@gmail.com>
2024-07-02 12:47:16 +0530

    second-commits

---
+++ ES-Modules/main-esm.mjs
@@ -0,0 +1,36 @@
+// const add = (a, b) => {
+//   return a + b;
+// };
+
+// export default add;
+
+//2
+// export default (a, b) => {
+//   return a + b;
+// };
+
+//3
+
+// const add = (a, b) => {
+//   return a + b;
+// };
+
+// const subtarct = (a, b) => {
+//   return a - b;
+// };
+
+//4
+export const add = (a, b) => {
+  return a + b;
+};
+
+export const subtarct = (a, b) => {
+  return a - b;
+};
+
+// export default {
+//   // add: add,
+//   // subtarct: subtarct
+//   add,
+//   subtarct,
+// };
---
+++ ES-Modules/main.mjs
@@ -0,0 +1,12 @@
+// import add from "./main-esm.mjs";
+// import math from "./main-esm.mjs";
+import * as math from "./main-esm.mjs";
+import { add, subtarct } from "./main-esm.mjs";
+
+// console.log(add(6, 8));
+
+// const { add, subtarct } = math;
+
+// console.log(math.add(3, 6), math.subtarct(2, 7));
+
+console.log(add(3, 6), subtarct(2, 7));
