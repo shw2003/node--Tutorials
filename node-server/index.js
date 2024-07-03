21b7f83a855fd39c293cd778a6e20573e465fd4c
shweta2003 <shwetakamal4714@gmail.com>
2024-07-03 15:40:31 +0530

    3july-commits

--- node-server/index.js
+++ node-server/index.js
@@ -1,8 +1,16 @@
 const http = require("node:http");
 
+const fs = require("node:fs");
+
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("hello world");
+  const superHero = {
+    firstName: "bruce",
+    lastName: "wayne",
+  };
+  res.writeHead(200, { "Content-Type": "text/html" });
+  //   res.end(JSON.stringify(superHero));
+  const html = fs.readFileSync("./index.html", "utf-8");
+  res.end(html);
 });
 
 server.listen(3000, () => {
